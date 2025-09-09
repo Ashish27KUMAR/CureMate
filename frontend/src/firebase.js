c17b4a50f828145a52ae0167aeadd9e09432aeb5
Ashish Kumar <rishukumarchaurasiya44@gmail.com>
2025-09-09 13:35:39 +0530

    firebase.js bug fixed

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,13 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; // Add GoogleAuthProvider import
 import { getFirestore } from "firebase/firestore";
+
+// Load environment variables
 const firebaseApiKey = import.meta.env.VITE_FIREBASE_API_KEY;
 const firebaseAuthDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
 const firebaseProjectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
 const firebaseStorageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
 const firebaseMessagingSenderId = import.meta.env
   .VITE_FIREBASE_MESSAGING_SENDER_ID;
-const firebaseAppId = import.meta.env.VITE.VITE_FIREBASE_APP_ID;
+const firebaseAppId = import.meta.env.VITE_FIREBASE_APP_ID;
+
+// Check if all required environment variables are present
+if (
+  !firebaseApiKey ||
+  !firebaseAuthDomain ||
+  !firebaseProjectId ||
+  !firebaseStorageBucket ||
+  !firebaseMessagingSenderId ||
+  !firebaseAppId
+) {
+  throw new Error("Firebase environment variables are not properly set.");
+}
 
 // Firebase config
 const firebaseConfig = {
